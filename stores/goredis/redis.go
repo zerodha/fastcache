afacf9d9bd054696cbbe87b0450adcd21a13c0de
Rohan Verma <hello@rohanverma.net>
2024-08-06 15:33:22 +0530

    feat: add support for async redis writes

--- stores/goredis/redis.go
+++ stores/goredis/redis.go
@@ -37,19 +37,46 @@ const (
 
 // Store is a Redis cache store implementation for fastcache.
 type Store struct {
-	prefix string
+	config Config
+	putBuf chan putReq
 	cn     redis.UniversalClient
 	ctx    context.Context
 }
 
+type Config struct {
+	// Prefix is the prefix to apply to all cache keys.
+	Prefix string
+
+	// Async enables async writes to Redis. If enabled, writes are buffered
+	// and committed in batches.
+	Async bool
+	// AsyncMaxCommitSize is the maximum number of writes to commit in a single
+	// batch.
+	AsyncMaxCommitSize int
+	// AsyncBufSize is the size of the write buffer, i.e. the channel size for
+	// async writes.
+	AsyncBufSize int
+	// AsyncBufTimeout is the maximum time to wait before committing the write
+	// buffer.
+	AsyncBufTimeout time.Duration
+}
+
 // New creates a new Redis instance. prefix is the prefix to apply to all
 // cache keys.
-func New(prefix string, client redis.UniversalClient) *Store {
-	return &Store{
-		prefix: prefix,
+func New(cfg Config, client redis.UniversalClient) *Store {
+	s := &Store{
+		config: cfg,
 		cn:     client,
 		ctx:    context.TODO(),
 	}
+
+	// Start the async worker if enabled.
+	if cfg.Async {
+		s.putBuf = make(chan putReq, s.config.AsyncBufSize)
+		go s.putWorker()
+	}
+
+	return s
 }
 
 // Get gets the fastcache.Item for a single cached URI.
@@ -99,8 +126,25 @@ func (s *Store) Get(namespace, group, uri string) (fastcache.Item, error) {
 	return out, err
 }
 
+type putReq struct {
+	namespace string
+	group     string
+	uri       string
+	b         fastcache.Item
+	ttl       time.Duration
+}
+
 // Put sets a value to given session but stored only on commit
 func (s *Store) Put(namespace, group, uri string, b fastcache.Item, ttl time.Duration) error {
+	if s.config.Async {
+		s.putBuf <- putReq{namespace, group, uri, b, ttl}
+		return nil
+	}
+
+	return s.putSync(namespace, group, uri, b, ttl)
+}
+
+func (s *Store) putSync(namespace, group, uri string, b fastcache.Item, ttl time.Duration) error {
 	var (
 		key = s.key(namespace, group)
 		p   = s.cn.Pipeline()
@@ -128,6 +172,57 @@ func (s *Store) Put(namespace, group, uri string, b fastcache.Item, ttl time.Dur
 	return err
 }
 
+func (s *Store) putWorker() {
+	var (
+		p     = s.cn.Pipeline()
+		count = 0
+		timer = time.NewTimer(s.config.AsyncBufTimeout).C
+	)
+	for {
+		select {
+		case req := <-s.putBuf:
+			key := s.key(req.namespace, req.group)
+			if err := p.HMSet(s.ctx, key, map[string]interface{}{
+				s.field(keyCtype, req.uri):       req.b.ContentType,
+				s.field(keyEtag, req.uri):        req.b.ETag,
+				s.field(keyCompression, req.uri): req.b.Compression,
+				s.field(keyBlob, req.uri):        req.b.Blob,
+			}).Err(); err != nil {
+				continue
+			}
+
+			// Set a TTL for the group. If one uri in cache group sets a TTL
+			// then entire group will be evicted. This is a short coming of using
+			// hashmap as a group. Needs some work here.
+			if req.ttl.Seconds() > 0 {
+				if err := p.PExpire(s.ctx, key, req.ttl).Err(); err != nil {
+					continue
+				}
+			}
+
+			if count++; count > s.config.AsyncMaxCommitSize {
+				if _, err := p.Exec(s.ctx); err != nil {
+					// Log error.
+				}
+				count = 0
+				p = s.cn.Pipeline()
+			}
+
+		case <-timer:
+			if count > 0 {
+				if _, err := p.Exec(s.ctx); err != nil {
+					// Log error.
+				}
+				count = 0
+				p = s.cn.Pipeline()
+			}
+
+		case <-s.ctx.Done():
+			return
+		}
+	}
+}
+
 // Del deletes a single cached URI.
 func (s *Store) Del(namespace, group, uri string) error {
 	return s.cn.HDel(s.ctx, s.key(namespace, group),
@@ -151,7 +246,7 @@ func (s *Store) DelGroup(namespace string, groups ...string) error {
 }
 
 func (s *Store) key(namespace, group string) string {
-	return s.prefix + namespace + sep + group
+	return s.config.Prefix + namespace + sep + group
 }
 
 func (s *Store) field(key string, uri string) string {
